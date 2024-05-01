ebc70e4bd9d5da8162391e31f0776a6a94dae269
saurabhburade <sburade3@gmail.com>
2024-05-01 23:28:47 +0530

    basic decode runes

--- src/map_runes.rs
+++ src/map_runes.rs
@@ -6,7 +6,7 @@ use bitcoin::block::Version;
 use bitcoin::hashes::sha256d::Hash;
 use bitcoin::network::message_network::VersionMessage;
 use bitcoin::{ Amount, OutPoint, ScriptBuf, Sequence, Transaction, TxIn, Txid, Witness };
-use ordinals::{ Artifact, Runestone };
+use ordinals::{ Artifact, Edict, Runestone };
 
 use crate::pb::btc::transaction_meta::v1::{ Block };
 use crate::pb::btc::runes_meta::v1::{ Rune };
@@ -18,7 +18,7 @@ fn map_runes(blk: Block) -> Result<Rune, Vec<substreams::errors::Error>> {
 
     let mut rune_data: Rune = Rune::default();
     for trans in &blk.tx {
-        substreams::log::println("Inside  tx map");
+        // substreams::log::println("Inside  tx map");
 
         let inputs = trans.vin.iter().map(|_vin| {
             // let wt = _vin.txinwitness
@@ -52,7 +52,7 @@ fn map_runes(blk: Block) -> Result<Rune, Vec<substreams::errors::Error>> {
                 // ), // Convert Vec<String> to Witness
             }
         });
-        substreams::log::println(inputs.len().to_string());
+        // substreams::log::println(inputs.len().to_string());
 
         let outputs = trans.vout.iter().map(|_vout| {
             TxOut {
@@ -62,7 +62,7 @@ fn map_runes(blk: Block) -> Result<Rune, Vec<substreams::errors::Error>> {
                 value: _vout.value.clone() as u64,
             }
         });
-        substreams::log::println(outputs.len().to_string());
+        // substreams::log::println(outputs.len().to_string());
 
         let txnss = Transaction {
             version: 2,
@@ -77,13 +77,33 @@ fn map_runes(blk: Block) -> Result<Rune, Vec<substreams::errors::Error>> {
             if let Some(rs) = &rs {
                 if let Artifact::Runestone(runestone) = rs {
                     if let Some(etching) = &runestone.etching {
-                        if let Some(symbol) = &etching.rune {
-                            substreams::log::println(symbol.to_string());
+                        if let Some(terms) = &etching.terms {
+                            if let Some(cap) = &terms.cap {
+                                // substreams::log::println(cap.to_string());
+                            }
                         } else {
-                            substreams::log::println("Symbol is None".to_string());
+                            // substreams::log::println("Symbol is None".to_string());
                         }
-                    } else {
-                        substreams::log::println("Etching is None".to_string());
+                    }
+                    if let Some(pointer) = &runestone.pointer {
+                        substreams::log::println("pointer ::: AMT START".to_string());
+                        substreams::log::println(pointer.to_string());
+                        substreams::log::println(trans.txid.to_string());
+                        substreams::log::println("pointer ::: AMT END".to_string());
+                    }
+                    // if let Some(mint) = &runestone.mint {
+                    //     substreams::log::println("MINT ::: AMT START".to_string());
+                    //     substreams::log::println(mint.block.to_string());
+                    //     substreams::log::println(mint.tx.to_string());
+                    //     substreams::log::println(trans.txid.to_string());
+                    //     substreams::log::println("MINT ::: AMT END".to_string());
+                    // }
+                    for Edict { id, amount, output } in runestone.edicts.iter().copied() {
+                        // substreams::log::println("EDICTTT ::: AMT START".to_string());
+                        // substreams::log::println(id.to_string());
+                        // substreams::log::println(amount.to_string());
+                        // substreams::log::println(output.to_string());
+                        // substreams::log::println("EDICTTT ::: AMT END".to_string());
                     }
                     // if let etching = runestone.etching {
                     //     // You have the Runestone artifact here, use it as needed
@@ -102,7 +122,7 @@ fn map_runes(blk: Block) -> Result<Rune, Vec<substreams::errors::Error>> {
         // Do something with `element`
     }
 
-    substreams::log::println("Outside  tx map");
+    // substreams::log::println("Outside  tx map");
 
     Ok(Rune { name: "SOME RUNE".to_string(), symbol: "SS".to_string() })
 }
